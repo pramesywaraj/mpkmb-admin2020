3f4b9440e9c1a67f53f26631840c7247ec9d4555
iqbalchoironi <iqbal.choironi@gmail.com>
2020-08-28 05:26:16 +0700

    add utils for formated date

---
+++ src/Utils/date.js
@@ -0,0 +1,17 @@
+function formatDate(date) {
+	var d = new Date(date),
+		month = '' + (d.getMonth() + 1),
+		day = '' + d.getDate(),
+		year = d.getFullYear(),
+		hour = '' + d.getHours(),
+		minute = '' + d.getMinutes();
+
+	if (month.length < 2) month = '0' + month;
+	if (day.length < 2) day = '0' + day;
+	if (hour.length < 2) hour = '0' + hour;
+	if (minute.length < 2) minute = '0' + minute;
+
+	return [[day, month, year].join('-'), [hour, minute].join(':')].join(' ');
+}
+
+export default formatDate;
